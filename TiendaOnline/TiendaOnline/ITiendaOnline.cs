00e1fa39784d60ce224850f1e4853c6038902054
gaboramos <gabrielramossantos@outlook.com>
2017-12-02 09:06:42 -0400

    TiendaOnline WCF
    WCF

---
+++ TiendaOnline/TiendaOnline/ITiendaOnline.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TiendaOnline
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ITiendaOnline" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ITiendaOnline
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ TiendaOnline/TiendaOnline/TiendaOnline.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TiendaOnline
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "TiendaOnline" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione TiendaOnline.svc o TiendaOnline.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class TiendaOnline : ITiendaOnline
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
